705873bdfd4b3b7b85022e2008ab4da9fc473692
Wasim Raza <wasimraza1698@gmail.com>
2020-12-19 23:06:06 +0530

    first

---
+++ PharmacyMedicineSupplyApp/Helpers/IClient.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace PharmacyMedicineSupply.Helpers
+{
+    public interface IClient
+    {
+        public HttpClient GetAuthClient();
+        public HttpClient GetMedRepClient();
+        public HttpClient GetMedSupplyClient();
+        public HttpClient GetMedStockClient();
+    }
+}
---
+++ PharmacyMedicineSupplyApp/Providers/UserProvider.cs
@@ -0,0 +1,39 @@
+﻿using PharmacyMedicineSupply.Models;
+using System.Net.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using PharmacyMedicineSupply.Helpers;
+
+namespace PharmacyMedicineSupply.Providers
+{
+    public class UserProvider : IUserProvider
+    {
+        private readonly log4net.ILog _log = log4net.LogManager.GetLogger(typeof(UserProvider));
+        private readonly HttpClient _httpClient;
+
+        public UserProvider(IClient authClient)
+        {
+            _httpClient = authClient.GetAuthClient();
+        }
+        public async Task<HttpResponseMessage> Login(User credentials)
+        {
+            try
+            {
+                var httpClient = new HttpClient();
+                StringContent content = new StringContent(JsonConvert.SerializeObject(credentials), Encoding.UTF8, "application/json");
+                var response = await _httpClient.PostAsync("User/Login", content);
+                return response;
+            }
+            catch (Exception e)
+            {
+                _log.Info("Error in UserProvider while logging in for user : "+credentials.UserName+" - " + e.Message);
+                throw;
+            }
+        }
+    }
+}
